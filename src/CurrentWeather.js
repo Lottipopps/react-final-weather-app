24144950c0112b0a7510593d98f09fc78e0a7704
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-07-16 00:24:28 +0200

    Fixed some imprecisions

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import FormattedDate from "./FormattedDate";
 import Icon from "./Icon";
 import CurrentTemperature from "./CurrentTemperature";
