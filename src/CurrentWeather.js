2423b8224599aba760caa8d2839f8207bc799b7b
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-07-15 15:41:18 +0200

    Fixed bug and added Meteocons in svg format.

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -4,12 +4,12 @@ import FormattedDate from "./FormattedDate";
 export default function CurrentWeather(props) {
   return (
     <div className="CurrentWeather">
-      <h1>{props.name}</h1>
+      <h1>{props.info.name}</h1>
       <ul>
         <li>
-          <FormattedDate dateInfo={props.date} />
+          <FormattedDate dateInfo={props.info.date} />
         </li>
-        <li className="text-capitalize">{props.description}</li>
+        <li className="text-capitalize">{props.info.description}</li>
       </ul>
       <div className="row">
         <div className="col-6">
@@ -17,13 +17,15 @@ export default function CurrentWeather(props) {
             src="https://cdn.dribbble.com/users/77598/screenshots/5602856/media/1e44f47cd47f97cd20e4d9fe42255a75.png"
             alt="temporary icon"
           />
-          <span className="temperature">{Math.round(props.temperature)}</span>
+          <span className="temperature">
+            {Math.round(props.info.temperature)}
+          </span>
           <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.humidity}%</li>
-            <li>Wind: {Math.round(props.wind)}km/h</li>
+            <li>Humidity: {props.info.humidity}%</li>
+            <li>Wind: {Math.round(props.info.wind)}km/h</li>
           </ul>
         </div>
       </div>
